5d4cf604a1d9ad74bd068cccacb3a8c7c6ed0241
Lynx <34606382+Fahad-M@users.noreply.github.com>
2018-09-29 23:19:27 +0500

    Update lratclient.py

--- lratclient.py
+++ lratclient.py
@@ -1,3 +1,4 @@
+# A Really Basic Reverse TCP Client.
 import socket
 import os
 from threading import Thread
@@ -19,7 +20,7 @@ def __main__():
     print("To view your Local IP Address..")
     print(" - On Unix/Linux :: Open Terminal & type 'ifconfig'. You will see it there.")
     print(" - On Windows :: Open Command Prompt & type 'ipconfig'. You will see it there as IPv4 Address."+Style.RESET_ALL)
-    host = input(Fore.LIGHTWHITE_EX+"[ ! ] Enter the LOCAL IP of the System Running LRAT Server : ")
+    host = input(Fore.LIGHTWHITE_EX+"[ ! ] Enter IP of the System Running LRAT Server : ")
     port = 415
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,8 +38,8 @@ def __main__():
             elif(data == "help"):
                 server.send("Basic Commands : sysinfo, time, lol, cool, contribute, troll. I will follow these.".encode())
             elif(data == "sysinfo"):
-                sysinfo = platform.version() + platform.uname() + platform.system() + platform.platform() 
-                server.send(sysinfo).encode()
+                sysinfo = platform.version() + platform.system() + platform.platform() 
+                server.send(sysinfo.encode())
             elif(data == "time"):
                 ostime = datetime.datetime.now()
                 ostime = str(ostime)
